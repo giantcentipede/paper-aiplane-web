1ce11454bd5877a9f7a5300405d98cbcff20564c
giantcentipede <97794708+giantcentipede@users.noreply.github.com>
2022-09-24 08:54:46 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,31 @@
+function q() {
+    window.location.replace("page1.html");
+}
+
+function w() {
+    window.location.replace("page2.html");
+}
+
+function e() {
+    window.location.replace("page3.html");
+}
+
+function r() {
+    window.location.replace("page4.html");
+}
+
+function t() {
+    window.location.replace("page5.html");
+}
+
+function y() {
+    window.location.replace("page6.html");
+}
+
+function back() {
+    window.location.replace("home.html");
+}
+
+function comment() {
+    window.location.replace("comments.html");
+}
